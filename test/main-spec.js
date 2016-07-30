7bb54225ff207ab74b171e2d99facf3104ae93df
justapps4all <dev@justapps4all.com>
2016-07-30 15:57:37 +0200

    GMT: Sat Jul 30 13:57:37 UTC 2016

--- test/main-spec.js
+++ test/main-spec.js
@@ -1,21 +1,9 @@
-if( typeof myModule === 'undefined' ) {
-    var require = require || null;
-    var window = window || null;
-    if(require != null){
-
-        var myModule;
-        if(window==null){
-            myModule = require('..')
-        }else{
-            myModule = GitHubJsonP;
-        }
-    }
-}
+var GitHubJsonP = require('../src/main.js');
 
 describe('mymodule', function(){
 
     it('something must be done', function(){
-        expect( myModule.VERSION ).toBe( '0.1.0' )
+        expect( GitHubJsonP.VERSION ).toBe( '0.1.0' )
     })
 
 })
