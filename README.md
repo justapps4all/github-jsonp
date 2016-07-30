1e67e04933f201b0e5fce43f4bf72e5f5b82bf41
justapps4all <dev@justapps4all.com>
2016-07-30 13:06:55 +0200

    GMT: Sat Jul 30 11:06:55 UTC 2016

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 [![Build Status](https://travis-ci.org/justapps4all/github-jsonp.svg?branch=master)](https://travis-ci.org/justapps4all/github-jsonp)
+[![Build Status](https://saucelabs.com/buildstatus/juanmadev)](https://saucelabs.com/beta/builds/fed505aa95534624a4e3863fdd5f7f70)
+[![Build Status](https://saucelabs.com/browser-matrix/juanmadev.svg)](https://saucelabs.com/beta/builds/fed505aa95534624a4e3863fdd5f7f70)
 
 GitHub minimal library with JSONP
 
--- karma.conf-sauce.js
+++ karma.conf-sauce.js
@@ -1,60 +1,9 @@
-var _ = require('underscore');
 var fs = require('fs');
-
-// Browsers to run on Sauce Labs platforms
-var sauceBrowsers = _.reduce([
-    ['firefox', '35'],
-    ['firefox', '30'],
-    ['firefox', '21'],
-    ['firefox', '11'],
-    ['firefox', '4'],
-
-    ['chrome', '40'],
-    ['chrome', '39'],
-    ['chrome', '31'],
-    ['chrome', '26'],
-
-    ['microsoftedge', '20.10240', 'Windows 10'],
-    ['internet explorer', '11', 'Windows 10'],
-    ['internet explorer', '10', 'Windows 8'],
-    ['internet explorer', '9', 'Windows 7'],
-    // Currently disabled due to karma-sauce issues
-    // ['internet explorer', '8'],
-    // ['internet explorer', '7'],
-    // ['internet explorer', '6'],
-
-    ['opera', '12'],
-    ['opera', '11'],
-
-    ['android', '5'],
-    ['android', '4.4'],
-    ['android', '4.3'],
-    ['android', '4.0'],
-
-    ['safari', '8.0', 'OS X 10.10'],
-    ['safari', '7'],
-    ['safari', '6'],
-    ['safari', '5']
-], function(memo, platform) {
-    // internet explorer -> ie
-    var label = platform[0].split(' ');
-    if (label.length > 1) {
-        label = _.invoke(label, 'charAt', 0)
-    }
-    label = (label.join("") + '_v' + platform[1]).replace(' ', '_').toUpperCase();
-    memo[label] = _.pick({
-        'base': 'SauceLabs',
-        'browserName': platform[0],
-        'version': platform[1],
-        'platform': platform[2]
-    }, Boolean);
-    return memo;
-}, {});
 
 module.exports = function(config) {
 
     // Use ENV vars on Travis and sauce.json locally to get credentials
-    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY ) {
+    if (!process.env.SAUCE_USERNAME) {
         if (!fs.existsSync('sauce.json')) {
             console.log('Create a sauce.json with your credentials based on the sauce-sample.json file.');
             process.exit(1);
@@ -64,38 +13,65 @@ module.exports = function(config) {
         }
     }
 
+    // Browsers to run on Sauce Labs
+    var customLaunchers = {
+        'SL_Chrome': {
+            base: 'SauceLabs',
+            browserName: 'chrome'
+        },
+        'SL_InternetExplorer': {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            version: '10'
+        },
+        'SL_FireFox': {
+            base: 'SauceLabs',
+            browserName: 'firefox',
+        }
+    };
+
     config.set({
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
-        singleRun: true,
+
 
         // list of files / patterns to load in the browser
         files: [
-            'test/vendor/qunit-extras.js',
-            'test/qunit-setup.js',
-            'underscore.js',
+            'src/*.js',
             'test/*.js'
         ],
 
-        // Number of sauce tests to start in parallel
-        concurrency: 9,
 
         // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
         reporters: ['dots', 'saucelabs'],
+
+
+        // web server port
         port: 9876,
+
         colors: true,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
+
         sauceLabs: {
-            build: 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')',
-            startConnect: true,
-            tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+            testName: 'Karma and Sauce Labs demo'
         },
-
         captureTimeout: 120000,
-        customLaunchers: sauceBrowsers,
+        customLaunchers: customLaunchers,
 
-        // Browsers to launch, commented out to prevent karma from starting
-        // too many concurrent browsers and timing sauce out.
-        browsers: _.keys(sauceBrowsers)
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: Object.keys(customLaunchers),
+        singleRun: true
     });
 };
