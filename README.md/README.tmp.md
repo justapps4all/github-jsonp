349a39ffc1b27ffaf194ac02bf7970d68979b157
justapps4all <dev@justapps4all.com>
2016-07-30 18:59:17 +0200

    [Prerelease] Bumped version number

---
+++ README.md/README.tmp.md
@@ -0,0 +1,8 @@
+[![Build Status](https://travis-ci.org/justapps4all/github-jsonp.svg?branch=master)](https://travis-ci.org/justapps4all/github-jsonp)
+[![Sauce Test Status](https://saucelabs.com/buildstatus/juanmadev)](https://saucelabs.com/u/juanmadev?Sat Jul 30 2016 18:59:17 GMT+0200 (CEST))
+
+GitHub minimal library with JSONP
+
+#COMPATIBILITY
+![compatibility matrix](https://saucelabs.com/browser-matrix/juanmadev.svg?Sat Jul 30 2016 18:59:17 GMT+0200 (CEST))
+
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var replace = require('gulp-replace');
+var runSequence = require('run-sequence');
+var git = require('gulp-git');
+
+
+gulp.task('templates', function(){
+    gulp.src(['README.tmp.md'])
+        .pipe(replace('TIMESTAMP', Date()))
+        .pipe(gulp.dest('README.md'));
+});
+
+gulp.task('commit-changes', function () {
+    return gulp.src('.')
+        .pipe(git.add())
+        .pipe(git.commit('[Prerelease] Bumped version number'));
+});
+
+gulp.task('push-changes', function (cb) {
+    git.push('origin', 'master', cb);
+});
+
+gulp.task('default', function (callback) {
+    runSequence(
+        'templates',
+        'commit-changes',
+        'push-changes',
+        function (error) {
+            if (error) {
+                console.log(error.message);
+            } else {
+                console.log('RELEASE FINISHED SUCCESSFULLY');
+            }
+            if(callback){
+                callback(error);
+            }
+        });
+});
